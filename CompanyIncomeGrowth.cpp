1d4e21be10ecd06acf1a49ce006cc6b819b05705
AddedAmine <168238027+AmineAdded@users.noreply.github.com>
2024-09-19 18:18:20 +0200

    Update CompanyIncomeGrowth.cpp

--- CompanyIncomeGrowth.cpp
+++ CompanyIncomeGrowth.cpp
@@ -2,33 +2,20 @@
 using namespace std;
 int main()
 {
-    int n,a,T[100];
+    int n,a,T[100],count=1,ans=0;
     cin>>n;
-    vector<int>V;
     for(int i=0;i<n;i++)
     {
         cin>>a;
-        V.push_back(a);
-    }
-    int ans=0,count=1;
-    for(int i=0;i<n;i++)
-    {
-        if(V[i]==count)
+        if(a==count)
         {
-            T[ans++]=2001+i;
+            T[ans++]=i+2001;
             count++;
         }
-    }
-    if(ans==0)
-    {
-        cout<<0<<endl;
-        return 0;
     }
     cout<<ans<<endl;
     for(int i=0;i<ans;i++)
     {
         cout<<T[i]<<" ";
     }
-
-
 }
