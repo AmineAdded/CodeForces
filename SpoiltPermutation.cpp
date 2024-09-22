8b91789429718d11ea5871893678104db3279607
AddedAmine <168238027+AmineAdded@users.noreply.github.com>
2024-09-22 16:51:37 +0200

    Update SpoiltPermutation.cpp

--- SpoiltPermutation.cpp
+++ SpoiltPermutation.cpp
@@ -1,37 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int n, a, l = 0, r = 0;
-    int T[1001];
-    
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> T[i];
-    }
-    
-    for (int i = 1; i <= n; i++) {
-        if (T[i] != i) {
-            if (l == 0) {
-                l = i;
-            }
-            r = i;
+int main()
+{
+    int n,a,l=0,r=0,T[1001],j;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a;
+        T[i]=a;
+        if(a!=i)
+        {
+            r=max(r,a);
+            if(l==0)
+                l=i;
         }
     }
-   
-    if (l == 0) {
-        cout << "0 0";
-        return 0;
-    }
-
-    for (int i = l, j = r; i <= r; i++, j--) {
-        if (T[i] != j) {
-            cout << "0 0";
+    for(int i=l,j=r;i<=r;i++,j--)
+    {
+        
+        if(T[i]!=j)
+        {
+            cout<<0<<" "<<0;
             return 0;
         }
     }
- 
-    cout << l << " " << r;
-    
-    return 0;
+    cout<<l<<" "<<r;
+   
 }
