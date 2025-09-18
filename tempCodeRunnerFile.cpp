40af8805aa1c7e688495be609c092f71f13b6444
Amine Added <amineadded3@gmail.com>
2025-09-18 11:24:27 +0200

    Tails_Ternary_Logic.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,17 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        int ind=i;
-        for(int j=1;j<=n;j++){
-            if(i!=j && i==ind){
-                cout<<j<<" ";
-                break;
-            } 
-            // else ind++;
-
-        }
-    }
-}
+cout<<sA<<" "<<sC<<endl;
