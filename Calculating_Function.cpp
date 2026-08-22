880939cc37860148e6f9e59c37c244076b77942b
Amine <addedamine@bizerte.r-iset.tn>
2026-08-22 14:17:16 +0100

    Calculating Function.cpp

---
+++ Calculating_Function.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n;
+    cin >> n;
+    if(n%2 == 0) {
+        cout << n/2 << endl;
+    }
+    else {
+        cout << n/2 -n << endl;
+    }
+}
